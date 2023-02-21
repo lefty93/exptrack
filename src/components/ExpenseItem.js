d26e6d4c219a92d5044636967917247e76d1ee7b
Lefty93 <yongchuseng@gmail.com>
2023-02-21 22:24:31 +0800

    Add Delete function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { RiDeleteBin2Fill } from "react-icons/ri";
+import { AppContext } from "../context/AppContext";
 
 function ExpenseItem(props) {
+  const { dispatch } = useContext(AppContext);
+
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: props.id
+    })
+  }
+
   return (
     <li className='px-2 py-3 border-b border-gray-200 flex justify-between'>
       {props.name}: RM{props.cost}
-      <RiDeleteBin2Fill size='1.5em'></RiDeleteBin2Fill>
+      <RiDeleteBin2Fill size='1.5em' onClick={handleDeleteExpense}></RiDeleteBin2Fill>
     </li>
   );
 }
